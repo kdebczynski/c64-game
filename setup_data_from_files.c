b17c9a8f3986a096d91d12655fe526257f9d9ba6
Krzysztof Debczynski <krzysztof@debczynski.pl>
2025-05-09 11:51:09 +0200

    Load char colors and map

--- setup_data_from_files.c
+++ setup_data_from_files.c
@@ -1,20 +1,34 @@
 #include <c64.h>
 
-char *const SCREEN_POINTER = (char *)$0400;       // Pointer to screen data
-char *const SPRITE_POINTER_DATA = (char *)$2800;  // Pointer to sprite data
-char *const CHARSET_POINTER_DATA = (char *)$2000; // Pointer to chars data
+char *const SCREEN_POINTER = (char *)$0400; // Pointer to screen data
 
-__address($2800) char SPRITES[] = kickasm(resource "data/Sprites.bin") {{
+const int SPRITE_ADDRESS = $2100;  
+const int CHARSET_ADDRESS = $2000;
+const int CHARSET_COLORS_ADDRESS = $3000;
+const int MAP_ADDRESS = $3100;
+
+const int NUMBER_OF_SPRITES = 3;
+
+__address(SPRITE_ADDRESS) char SPRITES[] = kickasm(resource "data/Sprites.bin") {{
     .var sprite = LoadBinary("Sprites.bin")
     .fill sprite.getSize(), sprite.get(i)
 }};
 
-__address($2000) char CHARS[] = kickasm(resource "data/Chars.bin") {{
+__address(CHARSET_ADDRESS) char CHARS[] = kickasm(resource "data/Chars.bin") {{
     .var chars = LoadBinary("Chars.bin")
     .fill chars.getSize(), chars.get(i)
 }};
 
-const int NUMBER_OF_SPRITES = 3;
+__address(CHARSET_COLORS_ADDRESS) char CHARS_COLORS[] = kickasm(resource "data/CharAttribs.bin") {{
+    .var charsAttribs = LoadBinary("CharAttribs.bin")
+    .fill charsAttribs.getSize(), charsAttribs.get(i)
+}};
+
+__address(MAP_ADDRESS) char MAP[] = kickasm(resource "data/Map.bin") {{
+    .var map = LoadBinary("Map.bin")
+    .fill map.getSize(), map.get(i)
+}};
+
 
 void main() {
   initialize();
@@ -29,8 +43,10 @@ void main() {
 void initialize() {
   VICII->MEMORY = toD018(SCREEN_POINTER, CHARS);
   VICII->SPRITES_ENABLE = %00000111;
+  VICII->BORDER_COLOR = 0;
+  VICII->BG_COLOR = 0;
 
-  clearScreen();
+  initScreenByMap(MAP);
   initSprites(NUMBER_OF_SPRITES);
 
   VICII->SPRITE0_X = 60;
@@ -61,8 +77,11 @@ void initSprites(int numOfSprites) {
   }
 }
 
-void clearScreen() {
-  for (char *sc = SCREEN_POINTER; sc < SCREEN_POINTER + 40*25; sc++) {
-    *sc = 1;
+void initScreenByMap(char* map) {
+  for (int i = 0; i < 40*25; i++) {
+    byte mapValue = map[i];
+    
+    SCREEN_POINTER[i] = mapValue;
+    COLS[i] = CHARS_COLORS[mapValue];
   }
 }
